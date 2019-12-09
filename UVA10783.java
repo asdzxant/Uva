28640e86b57165c2f0333ed8a841375c07c80755
whjuan <geniu723@gmail.com>
2019-12-09 14:02:43 +0800

    add Uva 10783

---
+++ UVA10783.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class UVA10783 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+    	
+		int t = sc.nextInt();
+    	for(int kase = 1; kase <= t; kase++){
+    		int a = sc.nextInt();
+    		int b = sc.nextInt();
+    		
+    		int sum = 0;
+    		for(int i=a;i<=b;i++){
+    			if(i%2!=0)
+    				sum+=i;
+    		}
+    		
+    		System.out.printf("Case %d: %d\r\n", kase, sum);
+    	}
+	}
+
+}
