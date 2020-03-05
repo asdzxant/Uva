7f8c3ed08fac5e43c0b9687282fb44100492682e
whjuan <50222306+whjuan@users.noreply.github.com>
2020-03-05 16:07:18 +0800

    Update UVA10935.java

--- UVA10935.java
+++ UVA10935.java
@@ -14,18 +14,22 @@ public class UVA10935 {
 			for(int i=1;i<=n;i++)
 				list.add(i);
 				
-			String ans = "";
+			ArrayList<Integer> ans = new ArrayList<>();
 			while(list.size()>1){
 				int r = list.remove();
-				if(ans.length()==0)
-					ans = ans + " " + r;
-				else
-					ans = ans + ", " + r;
+				ans.add(r);
 					
 				list.add(list.remove());
 			}
 			
-			System.out.println("Discarded cards:"+ans);
+			System.out.print("Discarded cards:");
+			for(int i=0;i<ans.size();i++){
+				if(i==0)
+					System.out.print(" " + ans.get(i));
+				else
+					System.out.print(", " + ans.get(i));
+			}
+			System.out.println();
 			System.out.println("Remaining card: "+list.remove());
 		}
 	}
