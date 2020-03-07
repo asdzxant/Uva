2e1e671d8983c9b272dddbc222d2b01e28fd1f4a
asdzxant <58451139+asdzxant@users.noreply.github.com>
2020-03-07 17:00:56 +0800

    Update UVA10420.java

--- UVA10420.java
+++ UVA10420.java
@@ -3,31 +3,36 @@ public class UVA10420{
 	public static void main(String args []){
 		Scanner sc=new Scanner(System.in);
 		int t=sc.nextInt();
-		Set<String> l=new TreeSet<String>();
 		String c[]=new String[t];
 		sc.nextLine();
 		
 		for(int i=0;i<t;i++){
 			String s[]=sc.nextLine().trim().split(" ");
 			c[i]=s[0];
-			//System.out.println(s[0]);
-			l.add(s[0]);
-			
 		}
 		Arrays.sort(c);
-		String st[]=l.toArray(new String[l.size()]);
-		
-		for(int i=0;i<st.length;i++){
-			int count=0;
-			for(int j=0;j<t;j++){
-				if(st[i].equals(c[j])) count++;
-			}
-			System.out.println(st[i]+" "+count);	
+		/*for(int i=0;i<t;i++) {
+			System.out.print(c[i]+" ");
 		}
-			
-			
+		System.out.println();
+		System.out.println("--------");*/
 		
-		
+		int count=1;
+		String now=c[0];
+		for(int i=1;i<t;i++) {
+			if(c[i].equals(now)) {
+				count++;
+			}else {
+				System.out.println(now+" "+count);
+				now=c[i];
+				count=1;
+				
+			}
+			if(i==t-1) {
+				System.out.println(now+" "+count);
+			}
+			
+		}
 		
 	}
 }
