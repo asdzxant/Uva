42b941b185fe683257d488cd3e1374b1a31ab0e7
asdzxant <58451139+asdzxant@users.noreply.github.com>
2020-03-16 14:08:41 +0800

    Update UVA10415.java

--- UVA10415.java
+++ UVA10415.java
@@ -6,56 +6,50 @@ public class UVA10415 {
 		Scanner sc=new Scanner(System.in);
 		int T=sc.nextInt();
 		sc.nextLine();
-		String let[]= {"c0111001111",
-					"d0111001110",
-					"e0111001100",
-					"f0111001000",
-					"g0111000000",
-					"a0110000000",
-					"b0100000000",
-					"C0010000000",
-					"D1111001110",
-					"E1111001100",
-					"F1111001000",
-					"G1111000000",
-					"A1110000000",
-					"B1100000000",
-					};
 		
+		
+		String M[]= {"0111001111",
+					"0111001110",
+					"0111001100",
+					"0111001000",
+					"0111000000",
+					"0110000000",
+					"0100000000",
+					"0010000000",
+					"1111001110",
+					"1111001100",
+					"1111001000",
+					"1111000000",
+					"1110000000",
+					"1100000000",
+					};
+		char c[]= {'c','d','e','f','g','a','b','C','D','E','F','G','A','B'};
 		while(T-->0) {
-			int f[]=new int [10];
-			int now[]=new int[10];
-			count=new int[10];
-			char c[]=sc.nextLine().toCharArray();
-			for(int i=0;i<c.length;i++) {
-				now =f(c[i],let,now);
-				//System.out.println(Arrays.toString(count));
-			}
-			for(int i=0;i<count.length;i++) {
-				if(i==0) {
-					System.out.print(count[i]);
-				}else {
-					System.out.print(" "+count[i]);
+			int ans[]=new int[10];
+			Arrays.fill(ans,0);
+			char s[]=sc.nextLine().trim().toCharArray();
+			String now="0000000000";
+			for(int i=0;i<s.length;i++) {
+				for(int j=0;j<14;j++) {
+					if(c[j]==s[i]) {
+						for(int u=0;u<10;u++) {
+							if(now.charAt(u)=='0'&&M[j].charAt(u)=='1') {
+								ans[u]++;
+							}
+						}
+						now=M[j];
+						break;
+					}
 				}
 			}
-			System.out.println();
+			for(int i=0;i<10;i++) {
+				if(i==0) {
+					System.out.print(ans[i]);
+				}else {
+					System.out.print(" "+ans[i]);
+				}	
+			}
+			System.out.println();		
 		}
 	}
-	public static int []f(char c,String let[],int now[]) {
-		String str="";
-		for(int i=0;i<let.length;i++) {
-			if(c==let[i].charAt(0)) {
-				str=let[i].replaceAll("[^\\d]","");
-			}
-		}
-		//System.out.println(str);
-		for(int i=0;i<10;i++) {
-			if(now[i]==0&&str.charAt(i)=='1') {
-				count[i]+=1;
-			}
-			now[i]=str.charAt(i)-'0';
-		}
-		return now;
-	}
-
 }
